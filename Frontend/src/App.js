7831cf5437e49e8f287413dea0b1d95806ce0bd4
Tanish Agrawal <tanishagrawalofficial1@gmail.com>
2023-03-28 23:24:34 +0530

    Frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import MainHeader from './components/MainHeader/MainHeader'
+import Registration from './components/Registration'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <div>
+      {/* <Login /> */}
+      <MainHeader />
+      {/* <Registration/> */}
+      <Login />
+    </div>
+  )
+}
+
+export default App
