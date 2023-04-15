e5b3a3c4b3dcc52bc42f241ea4571d31c2b04b3b
Tanish Agrawal <tanishagrawalofficial1@gmail.com>
2023-04-15 17:25:56 +0530

    Logout Authentication and Download Functionality Added

--- Frontend/src/components/RadiologistDoctor.js
+++ Frontend/src/components/RadiologistDoctor.js
@@ -1,39 +1,44 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import "../components/css/Navigation.css";
-import "../components/css/Patient.css";
+import { Link } from 'react-router-dom'
+import '../components/css/Navigation.css'
+import '../components/css/Patient.css'
+import Button from './Button/Button'
 
 function RadiologistDoctor() {
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    window.location.href = '/login'
+  }
   return (
     <div>
-        <div>
-    <div class="welcome">
-<h1>Welcome  Doctor!</h1>
-<p>Thanks for visiting. Feel free to look around.</p>
-</div>
-</div>
-<div>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/doctor">Home</Link>
-          </li>
-          <li>
-            <Link to="/prescription">Prescription</Link>
-          </li>
-          <li>
-            <Link to="/reportdoctor">Report</Link>
-          </li>
-          <li>
-            <Link to="/about">Edit Profile</Link>
-          </li>
-          <li>
-            <Link to="/login">Logout</Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <div class="welcome">
+          <h1>Welcome Doctor!</h1>
+          <p>Thanks for visiting. Feel free to look around.</p>
+        </div>
       </div>
-</div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/doctor">Home</Link>
+            </li>
+            <li>
+              <Link to="/prescription">Prescription</Link>
+            </li>
+            <li>
+              <Link to="/reportdoctor">Report</Link>
+            </li>
+            <li>
+              <Link to="/about">Edit Profile</Link>
+            </li>
+            <li>
+              <Button onClick={handleLogout}>Logout</Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
 
